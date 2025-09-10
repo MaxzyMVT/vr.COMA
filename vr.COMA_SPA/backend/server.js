603461861d535a4b0a2be0a4d1275855d5be8aa9
Piriyakorn Suphakitcharoen <154685978+TangO-271@users.noreply.github.com>
2025-09-10 11:42:44 +0700

    Revert "Fix route of server.js"
    This reverts commit 7d9be7796e1652ca56f979541e7a83135ff49d81.

--- vr.COMA_SPA/backend/server.js
+++ vr.COMA_SPA/backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const apiRoutes = require("./route/api");
+const apiRoutes = require("./routes/api");
 
 // --- Initial Setup ---
 const app = express();
